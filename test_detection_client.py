e27fb5c3473c1e9ebad41c636c50fcaf17c1ed65
Swall0w <technext.jpn@gmail.com>
2018-09-22 00:13:19 +0900

    [update]: client.

--- test_detection_client.py
+++ test_detection_client.py
@@ -1,8 +1,10 @@
 from lib import detection_grpc
+from lib.detection import chainercv_parse
 
 
 if __name__ == '__main__':
-    client = detection_grpc.MLClient('localhost:8888')
+    client = detection_grpc.MLClient('localhost:8888',
+                                     parse=chainercv_parse)
 
     with open('dog.jpg', 'rb') as f:
         image = f.read()
